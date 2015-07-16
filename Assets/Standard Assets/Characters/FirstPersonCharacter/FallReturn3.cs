363cfe6fd7f34ae73699ea9121f2d93aceed4d96
RDCGameCamp <dan.whiteside@rdc.ab.ca>
2015-07-16 14:19:36 -0600

    The level scene has been added.

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/FallReturn3.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallReturn3 : MonoBehaviour {
+	public GameObject player;
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player"))
+		{
+			player.transform.position = new Vector3(68f, 9f, 67f);
+			print(transform.position.x);
+		}
+	}
+}
